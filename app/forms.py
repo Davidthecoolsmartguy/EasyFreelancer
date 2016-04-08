25cb8f019a4065d7856093d01ce7e44455d8ea66
Davidthecoolsmartguy <Davidthecoolsmartguy@gmail.com>
2016-04-07 23:25:00 -0700

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -6,9 +6,11 @@ class HourlyRateForm(Form):
     hours_worked = TextField('Hours Worked', [required()] )
     hourly_rate = TextField('Houly Rate', [required()])
     submit = SubmitField("submit")
-
+    save_invoice = SubmitField("Save Invoice")
+    
 class GuaranteedHoursForm(Form):
     guaranteed_rate = TextField('Guranteed Rate', [required()])
     guaranteed_hours = TextField('Guranteed Hours', [required()])
     actual_hours_worked = TextField('Actual Hours Worked', [required()])
     submit = SubmitField("submit")
+    save_invoice = SubmitField("Save Invoice")
